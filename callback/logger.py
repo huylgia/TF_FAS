e9dec36124d749f2efd402e2e3103b27301e8b32
huylgia <lghuyvn2000@gmail.com>
2023-04-06 09:48:26 +0000

    fix callback

--- callback/logger.py
+++ callback/logger.py
@@ -6,19 +6,18 @@ class CustomLogger(Callback):
         self.log = open(log_file, 'a')
 
     def on_epoch_begin(self, epoch, logs=None):
-        with open(self.log_file, 'a') as f:
-            f.write('Starting epoch {}'.format(epoch+1))
+        self.log.write('Starting epoch {}'.format(epoch+1))
 
     def on_train_batch_end(self, batch, logs=None):
         lr = self.model.optimizer._learning_rate(self.model.optimizer.iterations)
         logs['lr'] = lr
 
         content = create_content(logs)
-        f.write('\tStep {}: {}'.format(batch, content))
+        self.log.write('\tStep {}: {}'.format(batch, content))
 
     def on_test_end(self, logs=None):
-        content = write_content(logs)
-        f.write("\tEvaluate: {}".format(content))
+        content = create_content(logs)
+        self.log.write("\tEvaluate: {}".format(content))
 
 def create_content(logs):
     form_string = "{}: {:>1.4f}"
